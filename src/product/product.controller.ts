91c8af61f981685469128f87cb7731a2e2ce6e1b
Jyotendra <jyotendrasharma92@gmail.com>
2021-05-14 14:46:29 +0530

    removed direct dependency from fakerservice in products

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, HttpException, HttpStatus, Query } from '@nestjs/common';
 import { ApiQuery } from '@nestjs/swagger';
-import { FakeProductService } from 'src/fake-data/fake-product.service';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-    constructor(private fakeProductService: FakeProductService) { }
+    constructor(private productService: ProductService) { }
     
     
     @Get()
@@ -12,7 +12,7 @@ export class ProductController {
     @ApiQuery({ name: 'limit', required: false })
     @ApiQuery({ name: 'all', enum: ["false", "true"] })
     getProducts(@Query("page") page: number, @Query("limit") limit, @Query("all") all: boolean) {
-        return this.fakeProductService.getProducts(page, limit, all);
+        return this.productService.getProducts(page, limit, all);
         // throw new HttpException({
         //     message: 'This is a custom message',
         // }, HttpStatus.FORBIDDEN);
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { FakeProductService } from 'src/fake-data/fake-product.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+
+    constructor(private fakeProductService: FakeProductService){}
+    
+    getProducts(page: number, limit: number, all: boolean) {
+        return this.fakeProductService.getProducts(page, limit, all);
+    }
+}
