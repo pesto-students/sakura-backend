dba89388ee8bc53d5a6c13ffb61b3f9dd9472de1
Jyotendra <jyotendrasharma92@gmail.com>
2021-05-23 21:47:45 +0530

    added fake data services for promo

--- src/env.ts
+++ src/env.ts
@@ -12,3 +12,12 @@ if (process.env.NODE_ENV === PROD_ENV) {
 }
 
 export const APP_PORT = process.env["APP_PORT"];
+
+// Db configs
+
+export const DB_HOST=process.env["DB_HOST"];
+export const DB_PORT=process.env["DB_PORT"];
+export const DB_USER=process.env["DB_USER"];
+export const DB_PASS=process.env["DB_PASS"];
+export const DB_NAME=process.env["DB_NAME"];
+
